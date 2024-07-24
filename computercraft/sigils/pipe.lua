e18c427550c37867f2292accb27e49b946daa6f2
fechan <fred@fredchan.org>
2024-07-24 15:42:08 -0700

    implement basic fluid pipe transfer

--- computercraft/sigils/pipe.lua
+++ computercraft/sigils/pipe.lua
@@ -5,11 +5,44 @@
 ---@class Pipe
 
 local TransferCalculator = require('sigils.transfer-calculator')
+local PipeModeFluid = require('sigils.pipeModes.fluid')
 local Filter = require('sigils.filter')
 local LOGGER = require('sigils.logging').LOGGER
-local Utils  = require('sigils.utils')
 
+local function processFluidPipe (pipe, groupMap, missingPeriphs, filter)
+  local ok, transferOrders = pcall(
+    function ()
+      return PipeModeFluid.getTransferOrders(groupMap[pipe.from], groupMap[pipe.to], missingPeriphs, filter)
+    end
+  )
+
+  if ok then
+    local coros = {}
+    for _, order in ipairs(transferOrders) do
+      local fromPeriph = peripheral.wrap(order.from.periphId)
+      local coro = function ()
+        pcall(function ()
+          fromPeriph.pushFluid(order.to.periphId)
+        end)
+      end
+      table.insert(coros, coro)
+    end
+    parallel.waitForAll(unpack(coros))
+  else
+    LOGGER:warn("pipe.lua#processFluidPipe() caught error " .. transferOrders)
+  end
+end
+
+---Transfer items or fluids across a single pipe
+---@param pipe Pipe Pipe to transfer items or fluids across
+---@param groupMap table<string, Group> A table mapping Group IDs to Groups
+---@param missingPeriphs table<string, boolean> A set of peripherals that are missing
 local function processPipe (pipe, groupMap, missingPeriphs)
+  if groupMap[pipe.from].fluid then
+    processFluidPipe(pipe, groupMap, missingPeriphs, function () end)
+    return
+  end
+
   local filter = Filter.getFilterFn(pipe.filter)
   local ok, transferOrders = pcall(
     function ()
---
+++ computercraft/sigils/pipeModes/fluid.lua
@@ -0,0 +1,26 @@
+---Get the transfer orders needed to transfer fluids from the origin fluid tanks
+---to the destinations
+---@param origin Group Origin fluid group to transfer from
+---@param destination Group Destination fluid group to transfer to
+---@param missingPeriphs table<string, boolean> Set of missing peripherals by ID
+---@param filter function Filter function that accepts individual fluids returned by fluid_storage.tanks()
+local function getTransferOrders (origin, destination, missingPeriphs, filter)
+  local orders = {}
+
+  for _, originSlot in ipairs(origin.slots) do
+    if not missingPeriphs[originSlot.periphId] then
+      for _, destSlot in ipairs(destination.slots) do
+        if not missingPeriphs[destSlot.periphId] then
+          table.insert(orders, {from=originSlot, to=destSlot})
+          print('from', originSlot.periphId, 'to', destSlot.periphId)
+        end
+      end
+    end
+  end
+
+  return orders
+end
+
+return {
+  getTransferOrders = getTransferOrders,
+}
