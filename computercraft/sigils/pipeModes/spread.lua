92aa27facf2086b7b2ce4583789ffe4399679e0d
fechan <fred@fredchan.org>
2024-11-11 14:18:01 -0800

    redo spread implementation to match minecraft spread behavior

--- computercraft/sigils/pipeModes/spread.lua
+++ computercraft/sigils/pipeModes/spread.lua
@@ -1,18 +1,23 @@
 local ItemDetailAndLimitCache = require('sigils.ItemDetailAndLimitCache')
 
-local function getTotalDestSlotsAvailableForOriginSlot(inventoryInfo, originSlot, emptySlotQueue)
-  local originItem = inventoryInfo:GetItemDetail(originSlot)
-  local possibleSlotsFull = inventoryInfo:GetSlotsWithMatchingItems(destination, function (item)
-    return (item.name == originItem.name and
-      item.nbt == nil and
-      item.durability == nil and
-      item.maxCount > item.count
-    )
-  end)
+---Get the amount of items from the origin item stack that can be moved to the
+---destination item stack.
+---@param originStack table ItemDetail of the origin item stack
+---@param destStack table ItemDetail of the destination item stack
+---@return number Number Number of items that can be moved. 0 if destination full or unstackable.
+local function getAmountStackable(originStack, destStack)
+  local isCompatibleItem = (
+    destStack.name == originStack.name and
+    destStack.nbt == nil and
+    destStack.durability == nil and
+    destStack.maxCount > originStack.count
+  )
 
-  local emptySlotsAvailable = #emptySlotQueue
-
-  return emptySlotsAvailable + #possibleSlotsFull
+  if isCompatibleItem then
+    return destStack.maxCount - originStack.count
+  else
+    return 0
+  end
 end
 
 local function getTransferOrders (origin, destination, missingPeriphs, filter)
@@ -24,26 +29,26 @@ local function getTransferOrders (origin, destination, missingPeriphs, filter)
   local emptySlotQueue = inventoryInfo:GetEmptySlots()
 
   for _, originSlot in pairs(origin.slots) do
-    local totalDestSlotsAvailable = getTotalDestSlotsAvailableForOriginSlot(
-      inventoryInfo, originSlot, emptySlotQueue)
+    local originStack = inventoryInfo:GetItemDetail(originSlot) -- TODO: expecting this to be nil if the destSlot is empty. Is this true?
+    if originStack then
+      local toTransfer = originStack.count
+      local actualTransferred = 0
 
-    if totalDestSlotsAvailable == 0 then break end
+      local numDestinationSlots = min(slot_count, #destination.slots)
+      local transferAmount = floor(toTransfer / numDestinationSlots) -- we'll try to transfer this many items to each destination slot
 
-    local itemsPerDestSlot = inventoryInfo:GetNumExistingItemsAt(originSlot) / totalDestSlotsAvailable
-    local originStackSize = inventoryInfo:GetNumExistingItemsAt(originSlot)
+      for writeHead=1, numDestinationSlots do
+        local destSlot = destination.slots[writeHead]
+        local destStack = inventoryInfo:GetItemDetail(destSlot) -- TODO: expecting this to be nil if the destSlot is empty. Is this true?
 
-    -- reserve a certain number of slots equal to origin stack size // total available dest slots
-    for i=1, math.floor(originStackSize / totalDestSlotsAvailable) do
-      table.insert(order, {from=originSlot, to=possibleDestSlot, limit=originStackSize})
+        if destStack or getAmountStackable(originStack, destStack) then
+          local transferLimit = min(inventoryInfo:GetItemLimit(destSlot) - destStack.count, transferAmount)
+          actualTransferred = actualTransferred + transferLimit
+
+          table.insert(orders, {from=originSlot, to=destSlot, limit=transferLimit})
+        end
+      end
     end
-
-    -- if there's a remainder, reserve another slot and transfer the remainder
-    local itemsRemaining = itemsPerDestSlot % totalDestSlotsAvailable
-    table.insert(order, {from=originSlot, to=possibleDestSlot, limit=originStackSize})
-    if itemsRemaining then
-      table.insert(order, {from=originSlot, to=possibleDestSlot, limit=itemsRemaining})
-    end
-
   end
 
   return orders
