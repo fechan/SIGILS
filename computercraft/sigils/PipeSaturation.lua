24434d4bf0562d4b4f1e255eaebd66e9b9ab5bb8
fechan <fred@fredchan.org>
2025-06-09 23:13:46 -0700

    add pipe saturation abstraction

---
+++ computercraft/sigils/PipeSaturation.lua
@@ -0,0 +1,49 @@
+local PipeSaturation = {}
+
+local metatable = {
+  __eq = function (lhs, rhs)
+    return (
+      lhs.saturation == rhs.saturation and
+      lhs.degreeColorless == rhs.degreeColorless
+    )
+  end,
+
+  --- This function is REVERSED because pipe.lua uses a min heap, but we actually
+  --- want to pop the pipe with the max saturation
+  __lt = function (lhs, rhs)
+    if lhs.saturation > rhs.saturation then
+      return true
+    else
+      return lhs.degreeColorless > rhs.degreeColorless
+    end
+  end
+}
+
+function PipeSaturation.new (numNeighbors, pipeId)
+  local o = {
+    neighborColors = {},
+    saturation = 0,
+
+    degreeColorless = numNeighbors,
+
+    pipeId = pipeId
+  }
+
+  function o:addNeighborColor (color)
+    self.degreeColorless = degreeColorless - 1
+    if not neigborColors[color] then
+      neigborColors[color] = true
+      self.saturation = saturation + 1
+    end
+  end
+
+  function o:hasNeighborOfColor (color)
+    return self.neighborColors[color] ~= nil
+  end
+
+  setmetatable(o, metatable)
+
+  return o
+end
+
+return PipeSaturation
