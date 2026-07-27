137295eb8ade4ca3326e9c94a0f0e14863e015bc
fechan <fred@fredchan.org>
2026-07-26 22:40:18 -0700

    add some factory tests

---
+++ client/src/stores/factory.test.ts
@@ -0,0 +1,79 @@
+import { describe, expect, test } from 'vitest'
+import { Factory } from '@server/types/core-types.js';
+import { addGroups, addMachines, splitSlotFromGroup } from './factory.js';
+
+function getEmptyFactory() {
+  return {
+    machines: {},
+    pipes: {},
+    groups: {},
+    missing: {},
+    available: {},
+  } as Factory;
+}
+
+test("Adding a new group with a machine specified adds the group to machine's group list", () => {
+  const factory = getEmptyFactory();
+  addMachines(factory, [{
+    id: 'machine0',
+    groups: [],
+  }]);
+
+  addGroups(factory, [{
+    id: 'group0',
+    nickname: 'group0',
+    slots: [{periphId: 'periph0', slot: 0}],
+  }], 'machine0');
+
+  expect(factory.machines['machine0'].groups).contain('group0');
+});
+
+function getFactory_OneMachine_OneGroup_TwoSlots() {
+    const factory = getEmptyFactory();
+    addMachines(factory, [{
+      id: 'machine0',
+      groups: [],
+    }]);
+    addGroups(factory, [{
+      id: 'group0',
+      nickname: 'group0',
+      slots: [
+        {periphId: 'periph0', slot: 0},
+        {periphId: 'periph0', slot: 1},
+      ],
+    }], 'machine0');
+
+    return factory;
+}
+
+describe('splitSlotFromGroup', () => {
+  test('creates a new group in the same machine', () => {
+    const factory = getFactory_OneMachine_OneGroup_TwoSlots();
+
+    splitSlotFromGroup(factory, {periphId: 'periph0', slot: 1}, 'group0', 'machine0', 0, 0);
+
+    expect(factory.machines['machine0'].groups.length).toBe(2);
+    for (const groupId of factory.machines['machine0'].groups) {
+      if (groupId !== 'group0') {
+        expect(factory.groups[groupId].slots).toContainEqual({periphId: 'periph0', slot: 1});
+      }
+    }
+  });
+
+  test('removes the slot from the original group', () => {
+    const factory = getFactory_OneMachine_OneGroup_TwoSlots();
+    
+    splitSlotFromGroup(factory, {periphId: 'periph0', slot: 1}, 'group0', 'machine0', 0, 0);
+
+    expect(factory.groups['group0'])
+      .not.toContainEqual({periphId: 'periph0', slot: 1});
+  });
+
+  test('leaves the other slots in the old group', () => {
+    const factory = getFactory_OneMachine_OneGroup_TwoSlots();
+    
+    splitSlotFromGroup(factory, {periphId: 'periph0', slot: 1}, 'group0', 'machine0', 0, 0);
+
+    expect(factory.groups['group0'].slots).toContainEqual({periphId: 'periph0', slot: 0});
+  });
+});
