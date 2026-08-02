04362c98be3e2f0c1938e3f404a7b85ca2807e36
fechan <fred@fredchan.org>
2026-08-02 15:02:52 -0700

    add semicolon

--- client/src/stores/factory.test.ts
+++ client/src/stores/factory.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, test } from 'vitest'
+import { describe, expect, test } from 'vitest';
 import { Factory } from '@server/types/core-types.js';
 import { addGroups, addMachines, combineGroups, splitSlotFromGroup } from './factory.js';
 
