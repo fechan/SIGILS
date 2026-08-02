92644a13daf535fe17fcdd03a3a856b2954ca250
fechan <fred@fredchan.org>
2026-08-02 15:38:38 -0700

    add getAvailable and getMissing

--- client/src/stores/connectedPeriphs.ts
+++ client/src/stores/connectedPeriphs.ts
@@ -1,3 +1,4 @@
+import { PeriphId } from '@server/types/core-types';
 import { createStore } from 'zustand/vanilla';
 
 // TODO: this might become a core type
@@ -5,14 +6,42 @@ export type PeriphMap = {[periphName: string]: number};
 
 export interface ConnectedPeriphsStore {
   periphs: PeriphMap,
-  setPeriphs: (periphs: PeriphMap) => void
+  setPeriphs: (periphs: PeriphMap) => void,
+  getMissing: (periphsInFactory: Set<PeriphId>) => Set<PeriphId>,
+  getAvailable: (periphsInFactory: Set<PeriphId>) => Set<PeriphId>,
 };
 
 export function createConnectedPeriphsStore() {
   return createStore<ConnectedPeriphsStore>()(
-    (set) => ({
+    (set, get) => ({
       periphs: {},
       setPeriphs: (periphs) => set({periphs}),
+      getMissing: (periphsInFactory) => getMissing(get().periphs, periphsInFactory),
+      getAvailable: (periphsInFactory) => getAvailable(get().periphs, periphsInFactory),
     })
   );
+}
+
+function getMissing(connected: PeriphMap, periphsInFactory: Set<PeriphId>) {
+  const missing = new Set<PeriphId>();
+
+  for (const factoryPeriph in periphsInFactory) {
+    if (!(factoryPeriph in connected)) {
+      missing.add(factoryPeriph);
+    }
+  }
+
+  return missing;
+}
+
+function getAvailable(connected: PeriphMap, periphsInFactory: Set<PeriphId>) {
+  const available = new Set<PeriphId>();
+
+  for (const connectedPeriph in Object.keys(connected)) {
+    if (!periphsInFactory.has(connectedPeriph)) {
+      available.add(connectedPeriph);
+    }
+  }
+
+  return available;
 }
